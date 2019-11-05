8b23e047e90bf79d54a2c9bde01c583e07aaa7ab
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-11-05 15:26:02 +0700

    Update, seperate function for calculating score

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ import (
 const STOPFLAG = "QUIT"
 const OFFSET = 2
 const PICKOFFSET = 1
+const NPAIRS = 11
+const NTHROW = 6
+const NDICE = 4
+
 
 type GameData struct{
 	playerName string
@@ -40,11 +44,10 @@ var gameData = GameData{"",0}
 func main(){
 	welcomeMessage()
 	playGame()
-	//printScoreboard()
 }
 
-func rollDice(dice *[4]int){
-	for i := 0; i < 4;  i++ {
+func rollDice(dice *[NDICE]int){
+	for i := 0; i < NDICE;  i++ {
 		rand.Seed(time.Now().UnixNano() + int64(i))
 		(*dice)[i] = rand.Intn(6) + 1
 	}
@@ -63,17 +66,17 @@ func welcomeMessage(){
 	fmt.Println()
 }
 
-func printScoreboard(dataPairs [11]Pairs, dataThrowaway [6]Throwaway){
-	fmt.Printf("+----------+------+-------+   +-----------+-------+\n")
-	fmt.Printf("| Sum Pair | Mark | Score |   | Throwaway | Total |\n")
-	fmt.Printf("+----------+------+-------+   +-----------+-------+\n")
+func printScoreboard(dataPairs [NPAIRS]Pairs, dataThrowaway [NTHROW]Throwaway){
+	fmt.Printf("+----------+------+-------+   +-----------+------+\n")
+	fmt.Printf("| Sum Pair | Mark | Score |   | Throwaway | Mark |\n")
+	fmt.Printf("+----------+------+-------+   +-----------+------+\n")
 	
 	var i int = 0
-	for i < 11 {
-		if	i < 6 {
+	for i < NPAIRS {
+		if i < NTHROW {
 			fmt.Printf("|%-10d|%-6d|%-7d|   ",dataPairs[i].pairSum, dataPairs[i].numPlayed, dataPairs[i].scorePairs)
-			fmt.Printf("|%-11d|%-7d|\n", dataThrowaway[i].diceNumber, dataThrowaway[i].total)
-			fmt.Printf("+----------+------+-------+   +-----------+-------+\n")
+			fmt.Printf("|%-11d|%-6d|\n", dataThrowaway[i].diceNumber, dataThrowaway[i].total)
+			fmt.Printf("+----------+------+-------+   +-----------+------+\n")
 		} else {
 			fmt.Printf("|%-10d|%-6d|%-7d|\n",dataPairs[i].pairSum, dataPairs[i].numPlayed, dataPairs[i].scorePairs)
 			fmt.Printf("+----------+------+-------+\n")
@@ -85,9 +88,9 @@ func printScoreboard(dataPairs [11]Pairs, dataThrowaway [6]Throwaway){
 }
 
 func playGame(){
-	var dataPairs = [11]Pairs{
+	var dataPairs = [NPAIRS]Pairs{
 		Pairs{2, 100, 0, 0},
-		Pairs{3, 70, 0, 0},
+		Pairs{3, 70, 0, 0}, 
 		Pairs{4, 60, 0, 0},
 		Pairs{5, 50, 0, 0},
 		Pairs{6, 40, 0, 0},
@@ -99,7 +102,7 @@ func playGame(){
 		Pairs{12, 100, 0, 0},
 	}
 
-	var dataThrowaway = [6]Throwaway{
+	var dataThrowaway = [NTHROW]Throwaway{
 		Throwaway{1, 0},
 		Throwaway{2, 0},
 		Throwaway{3, 0},
@@ -108,9 +111,9 @@ func playGame(){
 		Throwaway{6, 0},
 	}
 
-	var dice [4]int
+	var dice [NDICE]int
 	var firstDice, secondDice int
-	var totalSumPairs int
+	var indexFromSum, scoreGained int
 
 	for !isThrowawayDone(dataThrowaway) {
 		rollDice(&dice)
@@ -121,7 +124,7 @@ func playGame(){
 		fmt.Printf("Pick 2 Dice: ")
 		fmt.Scan(&firstDice, &secondDice)
 
-		totalSumPairs = (dice[firstDice - PICKOFFSET] + dice[secondDice - PICKOFFSET]) - OFFSET
+		indexFromSum = findIndexFromSum(dice, firstDice, secondDice)
 
 		for i, die := range dice {
 			if i!=firstDice - PICKOFFSET && i!=secondDice-PICKOFFSET{
@@ -129,19 +132,44 @@ func playGame(){
 			}
 		}
 
-		var scores = -200
-		if dataPairs[totalSumPairs].numPlayed > 5 {
-			scores = dataPairs[totalSumPairs].scorePairs + dataPairs[totalSumPairs].scoreWeights
-		} 
+		calculateScore(indexFromSum, &dataPairs, &scoreGained)
+		calculateTotalScore(dataPairs)
 
-		dataPairs[totalSumPairs].scorePairs = dataPairs[totalSumPairs].scorePairs + scores
-		gameData.scoreTotal = gameData.scoreTotal + scores
-
-		dataPairs[totalSumPairs].numPlayed++	
-
+		fmt.Printf("You have picked %d and %d. Sum Pairs are %d. Gained %d points. Total Points: %d \n", dice[firstDice - PICKOFFSET], dice[secondDice - PICKOFFSET], indexFromSum ,scoreGained, gameData.scoreTotal)
 		printScoreboard(dataPairs, dataThrowaway)
 	}
 
+}
+
+func findIndexFromSum(dice [4]int, firstDice, secondDice int) int{
+	return (dice[firstDice - PICKOFFSET] + dice[secondDice - PICKOFFSET]) - OFFSET
+}
+
+func calculateScore(i int, dataPairs *[NPAIRS]Pairs, scoreGained *int){
+	var numPlayed, scorePairs, scoreWeights *int
+
+	numPlayed = &dataPairs[i].numPlayed
+	scorePairs = &dataPairs[i].scorePairs
+	scoreWeights = &dataPairs[i].scoreWeights
+
+	*numPlayed++
+	
+	if *numPlayed > 5 {
+		*scorePairs += *scoreWeights
+	} else if *numPlayed == 5 {
+		*scorePairs = 0
+	} else {
+		*scorePairs = -200
+	}
+
+	*scoreGained = *scorePairs
+}
+
+func calculateTotalScore(dataPairs [11]Pairs){
+	gameData.scoreTotal = 0
+	for _, pairs := range dataPairs {
+		gameData.scoreTotal += pairs.scorePairs
+	}
 }
 
 func isThrowawayDone(dataThrowaway [6]Throwaway)bool{
